9b0664bc46f64e436cb18525d483f7a2be6a347f
Viji M <vijaymgs@gmail.com>
2026-04-23 11:18:10 +0530

    contact-fixes

--- commit-push.bat
+++ commit-push.bat
@@ -1,8 +1,21 @@
 @echo off
+echo.
+echo ========================================
+echo  PROPERTISM -- COMMIT + PUSH
+echo ========================================
+echo.
 
- 
-git add .
-echo Committing...
-git commit -m "21042026-1034p"
+python manage.py collectstatic --noinput
+
+echo.
+git add -A
+
+set /p MSG=Commit message: 
+git commit -m "%MSG%"
 git push origin main
+
+echo.
+echo ========================================
+echo  DONE -- Deployed via GitHub Actions
+echo ========================================
 echo.
